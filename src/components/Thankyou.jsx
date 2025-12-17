dca8b280f27ef2eca7fcce4c6ba3e1eeeb2864d9
syspreevimeoid-prog <syspreevimeoid@gmail.com>
2025-12-17 15:55:14 +0530

    conatct form submition code updated in herp section

---
+++ src/components/Thankyou.jsx
@@ -0,0 +1,14 @@
+export default function ThankYou() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center px-4">
+      <div>
+        <h1 className="text-4xl font-bold text-emerald-700 mb-4">
+          Thank You!
+        </h1>
+        <p className="text-gray-600">
+          Your enquiry has been sent successfully. We’ll contact you shortly.
+        </p>
+      </div>
+    </div>
+  );
+}
